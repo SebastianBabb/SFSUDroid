ee2df0615ef6ad31d6c0e4811500161d2e9afd49
haichuand <haichuand@gmail.com>
2016-04-15 11:52:28 -0700

    Implemented ClassCatalog UI with demo data

--- app/src/main/java/woverines/sfsuapp/database/Course.java
+++ app/src/main/java/woverines/sfsuapp/database/Course.java
@@ -13,9 +13,13 @@ public class Course {
 
     private int    id;
     private String number;
+    private String section;
+    private String department;
     private String name;
     private String instructor;
     private String meetTime;
+    private String meetDays;
+    private String meetRoom;
     private String description;
 
 
@@ -40,7 +44,6 @@ public class Course {
         this.meetTime = "00:00";
         this.description = "blahblah";
     }
-
     public Course(int id, String number, String name, String instructor, String meetTime, String description)
     {
         this.id = id;
@@ -48,6 +51,20 @@ public class Course {
         this.name = name;
         this.instructor = instructor;
         this.meetTime = meetTime;
+        this.description = description;
+    }
+
+    public Course(int id, String department, String number, String section, String name, String meetDays, String meetTime, String meetRoom, String instructor, String description)
+    {
+        this.id = id;
+        this.department = department;
+        this.number = number;
+        this.section = section;
+        this.name = name;
+        this.meetDays = meetDays;
+        this.meetTime = meetTime;
+        this.meetRoom = meetRoom;
+        this.instructor = instructor;
         this.description = description;
     }
 
@@ -104,9 +121,67 @@ public class Course {
     //GETTERS
     public int    getId() {return this.id;}
     public String getNumber() {return this.number;}
+
+    public String getSection() {
+        return section;
+    }
+
     public String getName() {return this.name;}
     public String getInstructor() {return this.instructor;}
     public String getMeetTime() {return this.meetTime;}
     public String getDescription() {return this.description;}
 
+    public String getDepartment() {
+        return department;
+    }
+
+    public String getMeetDays() {
+        return meetDays;
+    }
+
+    public String getMeetRoom() {
+        return meetRoom;
+    }
+
+//SETTERS
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public void setSection(String section) {
+        this.section = section;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setInstructor(String instructor) {
+        this.instructor = instructor;
+    }
+
+    public void setMeetTime(String meetTime) {
+        this.meetTime = meetTime;
+    }
+
+    public void setMeetDays(String meetDays) {
+        this.meetDays = meetDays;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setMeetRoom(String meetRoom) {
+        this.meetRoom = meetRoom;
+    }
 }
