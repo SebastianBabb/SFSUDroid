9c524e8c836b753a5fcfd4bdbdf3e79c177ebe28
andreybarsukov <andr3y3@gmail.com>
2016-04-17 22:48:41 -0700

    Fixed bug, was using wrong ic_ images.

--- app/src/main/java/woverines/sfsuapp/activity/MainActivity.java
+++ app/src/main/java/woverines/sfsuapp/activity/MainActivity.java
@@ -54,7 +54,7 @@ public class MainActivity extends AppCompatActivity
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(mViewPager);
         //HIDES TABs
-        
+
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
