b2ceb99783177667e2f12c4185ec883b6e987973
Lowell Milliken <lmilliken@comcast.net>
2016-04-18 14:39:38 -0700

    Campus Map with pathfinding line (currently only dummy path from TH to Cesar Chavez)

--- app/src/main/java/woverines/sfsuapp/Maps/MapPathFinderAStar.java
+++ app/src/main/java/woverines/sfsuapp/Maps/MapPathFinderAStar.java
@@ -1,5 +1,6 @@
 package woverines.sfsuapp.Maps;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -11,7 +12,20 @@ public class MapPathFinderAStar implements MapPathfinder {
 
     @Override
     public List<MapNode> getPath() {
-        return null;
+        // Dummy Path
+        List<MapNode> path = new ArrayList<>();
+
+        path.add(new MapNode(37.72347404032161, -122.47694313526152, null));
+        path.add(new MapNode(37.72339633833966, -122.47693005949259, null));
+        path.add(new MapNode(37.7233931560042, -122.47706685215235, null));
+        path.add(new MapNode(37.72334860329329, -122.47712317854167, null));
+        path.add(new MapNode(37.72311496631688, -122.47731965035202, null));
+        path.add(new MapNode(37.72291182619023, -122.47771427035332, null));
+        path.add(new MapNode(37.722680309413605,-122.47794292867184,null));
+        path.add(new MapNode(37.72237347564042, -122.47804015874864, null));
+        path.add(new MapNode(37.722165294698954, -122.47845087200405, null));
+
+        return path;
     }
 
     @Override
--- app/src/main/java/woverines/sfsuapp/fragment/CampusMapFragment.java
+++ app/src/main/java/woverines/sfsuapp/fragment/CampusMapFragment.java
@@ -4,28 +4,52 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
+import woverines.sfsuapp.Maps.MapData;
+import woverines.sfsuapp.Maps.MapNode;
+import woverines.sfsuapp.Maps.MapPathFinderAStar;
+import woverines.sfsuapp.Maps.MapPathfinder;
+import woverines.sfsuapp.R;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
+import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolygonOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 
-import woverines.sfsuapp.R;
-
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class CampusMapFragment extends Fragment implements OnMapReadyCallback {
+public class CampusMapFragment extends Fragment implements OnMapReadyCallback, OnMapClickListener,
+        OnMarkerClickListener, AdapterView.OnItemSelectedListener{
     /**
      * The fragment argument representing the section number for this
      * fragment.
      */
     private static final String ARG_SECTION_NUMBER = "section_number";
     private static final String ARG_TEXT = "text";
+    private static final LatLng SFSU = new LatLng(37.7219, -122.4782);
+    private static final int STARTING_ZOOM = 17;
 
     private GoogleMap gMap;
 
@@ -67,12 +91,72 @@ public class CampusMapFragment extends Fragment implements OnMapReadyCallback {
         transaction.replace(R.id.campus_map, mapFragment, "map_fragment");
         transaction.commit();
 
+        Spinner spinner = (Spinner) rootView.findViewById(R.id.building_spinner);
+
+        CharSequence [] buildings = {"Select a Building","Thornton Hall","Cesar Chavez Student Center"};
+
+        ArrayAdapter<CharSequence> adapter = new ArrayAdapter<>(getContext(),
+                R.layout.support_simple_spinner_dropdown_item, buildings);
+
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
         return rootView;
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         gMap = googleMap;
+
+        Log.d("Testing Map ready", "testing");
+
+        gMap.setOnMapClickListener(this);
+        gMap.setOnMarkerClickListener(this);
+        gMap.moveCamera(CameraUpdateFactory.newLatLngBounds(
+                        new LatLngBounds(new LatLng(37.72083239241096,-122.48518355190754),
+                                            new LatLng(37.72567087764266,-122.47505284845829)), 0));
+
+        //gMap.moveCamera(CameraUpdateFactory.newCameraPosition((new CameraPosition.Builder().tilt(30).build())));
+    }
+
+    @Override
+    public void onMapClick(LatLng latLng) {
+        Log.i("Location", latLng.toString());
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        Log.i("Location", marker.getPosition().toString());
+        return false;
+    }
+
+    public Polyline drawPath(List<MapNode> path) {
+        PolylineOptions pathOptions = new PolylineOptions();
+
+        gMap.addMarker(new MarkerOptions().position(new LatLng(path.get(0).getX(),path.get(0).getY()))
+                                          .title("You are here"));
+        for(MapNode node : path) {
+            pathOptions.add(new LatLng(node.getX(), node.getY()));
+        }
+
+        return gMap.addPolyline(pathOptions);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+        if(position != 0) {
+            String building = (String) parent.getItemAtPosition(position);
+
+            MapPathfinder pathfinder = new MapPathFinderAStar();
+
+            pathfinder.setMapData(new MapData());
+
+            drawPath(pathfinder.getPath());
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
 
     }
 }
