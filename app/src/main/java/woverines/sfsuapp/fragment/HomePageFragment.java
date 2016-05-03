9d313b0d39499144c991d5a42a40ae12ec5e0820
andreybarsukov <andr3y3@gmail.com>
2016-05-02 19:40:29 -0700

    Tried several diff Twitter APIs, non currently working with fragment views. Webview also not working. using placeholder image for demo

--- app/src/main/java/woverines/sfsuapp/fragment/HomePageFragment.java
+++ app/src/main/java/woverines/sfsuapp/fragment/HomePageFragment.java
@@ -1,11 +1,17 @@
 package woverines.sfsuapp.fragment;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.TextView;
+
+import java.util.WeakHashMap;
 
 import woverines.sfsuapp.R;
 
@@ -44,8 +50,33 @@ public class HomePageFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_home_page, container, false);
         Bundle args = getArguments();
-        TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-        textView.setText(args.getString(ARG_TEXT));
+//        TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+//        textView.setText(args.getString(ARG_TEXT));
+
+
+        /*
+        //WEB VIEW
+        WebView webView = (WebView) rootView.findViewById(R.id.twitter_web);
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        if (Build.VERSION.SDK_INT >= 19) {
+            webView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+        }
+        else {
+            webView.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+        }
+
+        //disable links on webView
+        webView.setFocusableInTouchMode(false);
+        webView.setFocusable(false);
+        webView.setWebViewClient(new WebViewClient() {
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                return true;
+            }
+        });
+
+        webView.loadUrl("https://twitter.com/sfsu");
+        */
         return rootView;
     }
 }
