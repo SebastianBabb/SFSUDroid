387dcd643987f541950be0153d224d2881ffbf34
Josh Rubin <jkrubin42@gmail.com>
2016-05-07 01:21:28 -0700

    added image for Shuttl map

--- app/src/main/java/woverines/sfsuapp/fragment/ShuttleScheduleFragment.java
+++ app/src/main/java/woverines/sfsuapp/fragment/ShuttleScheduleFragment.java
@@ -8,12 +8,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 import woverines.sfsuapp.R;
 
@@ -21,15 +15,13 @@ import woverines.sfsuapp.R;
 /**
  * A placeholder fragment containing a simple view.
  */
-public class ShuttleScheduleFragment extends Fragment implements OnMapReadyCallback{
+public class ShuttleScheduleFragment extends Fragment{
     /**
      * The fragment argument representing the section number for this
      * fragment.
      */
     private static final String ARG_SECTION_NUMBER = "section_number";
     private static final String ARG_TEXT = "text";
-
-    private GoogleMap mMap;
 
     public ShuttleScheduleFragment() {
     }
@@ -60,24 +52,8 @@ public class ShuttleScheduleFragment extends Fragment implements OnMapReadyCallb
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
 
         FragmentManager manager = getChildFragmentManager();
-        SupportMapFragment mapFragment = (SupportMapFragment) manager.
-                findFragmentById(R.id.shuttleMapFragment);
-        if (mapFragment == null) {
-            mapFragment = SupportMapFragment.newInstance();
-            mapFragment.getMapAsync(this);
-        }
-
 
         return rootView;
     }
 
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-    }
 }
