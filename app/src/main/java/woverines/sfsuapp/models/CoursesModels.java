19d1255bc11d4fe17d746a7d227f1056161afd77
ironsquishy <spacecore88@gmail.com>
2016-05-10 14:46:13 -0700

    Checkpint with model

--- app/src/main/java/woverines/sfsuapp/models/CoursesModels.java
+++ app/src/main/java/woverines/sfsuapp/models/CoursesModels.java
@@ -7,8 +7,11 @@ public class CoursesModels {
     public List<Course> classes;
 
     public class Course {
+        public int course_id;
         public int course_number;
         public String course_description;
         public String course_name;
+        public String course_time;
+        //TODO ADD more stuff.
     }
 }
