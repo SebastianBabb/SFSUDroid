6482587b3fcbeb558e1340a9073b381baa9ae57d
Josh Rubin <jkrubin42@gmail.com>
2016-05-12 19:00:27 -0700

    Added hardcoded countdown. Updating to fit schedule

--- app/src/main/java/woverines/sfsuapp/fragment/ShuttleScheduleFragment.java
+++ app/src/main/java/woverines/sfsuapp/fragment/ShuttleScheduleFragment.java
@@ -1,15 +1,18 @@
 package woverines.sfsuapp.fragment;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewManager;
-import android.widget.ImageView;
+import android.widget.Button;
 import android.widget.TextView;
 
+
+import java.util.Calendar;
+import java.util.concurrent.TimeUnit;
 
 import woverines.sfsuapp.R;
 
@@ -17,6 +20,7 @@ import woverines.sfsuapp.R;
 /**
  * A placeholder fragment containing a simple view.
  */
+
 public class ShuttleScheduleFragment extends Fragment{
     /**
      * The fragment argument representing the section number for this
@@ -24,6 +28,9 @@ public class ShuttleScheduleFragment extends Fragment{
      */
     private static final String ARG_SECTION_NUMBER = "section_number";
     private static final String ARG_TEXT = "text";
+    private Button timeButton;
+    private TextView timerView;
+    private Timer countdown;
 
     public ShuttleScheduleFragment() {
     }
@@ -49,15 +56,63 @@ public class ShuttleScheduleFragment extends Fragment{
         View rootView = inflater.inflate(R.layout.fragment_shuttle_schedule, container, false);
         Bundle args = getArguments();
         final TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-        textView.setText(args.getString(ARG_TEXT));
+        timeButton = (Button) rootView.findViewById(R.id.ShuttleButton);
+        timerView = (TextView) rootView.findViewById(R.id.timerText);
 
+        countdown = new Timer(180000,1000);
+        makeNewTimer();
+        timeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                makeNewTimer();
+                countdown.start();
+            }
+        });
 
-        //ImageView shuttleView = (ImageView)getView().findViewById(R.id.ShuttleMapFrame);
-        //shuttleView.setOnTouchListener(this);
 
         return rootView;
     }
 
+    public void makeNewTimer(){
+        Calendar c = Calendar.getInstance();
+        long ms = TimeUnit.HOURS.toMillis(c.get(Calendar.HOUR_OF_DAY))+
+                TimeUnit.MINUTES.toMillis(c.get(Calendar.MINUTE))+
+                TimeUnit.SECONDS.toMillis(c.get(Calendar.SECOND));
+
+
+    }
+
+    public class Timer extends CountDownTimer{
+
+        /**
+         * @param millisInFuture    The number of millis in the future from the call
+         *                          to {@link #start()} until the countdown is done and {@link #onFinish()}
+         *                          is called.
+         * @param countDownInterval The interval along the way to receive
+         *                          {@link #onTick(long)} callbacks.
+         */
+        public Timer(long millisInFuture, long countDownInterval) {
+            super(millisInFuture, countDownInterval);
+        }
+
+        @Override
+        public void onTick(long millisUntilFinished) {
+            long msRemaining, hrRemaining, minRemaining, secRemaining;
+
+            msRemaining = millisUntilFinished;
+            hrRemaining = TimeUnit.MILLISECONDS.toHours(msRemaining);
+            minRemaining = TimeUnit.MILLISECONDS.toMinutes(msRemaining) - 60*hrRemaining;
+            secRemaining = TimeUnit.MILLISECONDS.toSeconds(msRemaining) - 60*minRemaining;
+            String timeString = String.format("%02d:%02d:%02d",hrRemaining,minRemaining,secRemaining);
+            timerView.setText(timeString);
+
+        }
+
+        @Override
+        public void onFinish() {
+            timerView.setText("00:00:00");
+        }
+    }
 
 
 }
