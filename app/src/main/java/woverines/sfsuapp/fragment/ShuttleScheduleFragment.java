9272af6d9e10caeb74bc8eb7fd2ef3c595a320bd
Josh Rubin <jkrubin42@gmail.com>
2016-05-17 18:33:26 -0700

    added comments for ShuttleScheduleFragment java file

--- app/src/main/java/woverines/sfsuapp/fragment/ShuttleScheduleFragment.java
+++ app/src/main/java/woverines/sfsuapp/fragment/ShuttleScheduleFragment.java
@@ -34,15 +34,21 @@ public class ShuttleScheduleFragment extends Fragment{
      */
     private static final String ARG_SECTION_NUMBER = "section_number";
     private static final String ARG_TEXT = "text";
+
+    //Create Button vars
     private Button timeButton;
-    private TextView timerView;
-    private TextView timerDescription;
-    private Dialog mapInfoDialog;
     private Button showDialog;
     private Button dismissDialog;
+    //Create TextView vars
+    private TextView timerView;
+    private TextView timerDescription;
+    //Create Shuttle Schedule Dialog var
+    private Dialog mapInfoDialog;
+    //Create WebView for zoomed Static Map
     public WebView mapWeb;
-    private TextView daySchedule;
+    //Create Timer for NextShuttle timer
     private Timer countdown;
+    //Create Calender to create dynamic Timer
     private Calendar c;
 
 
@@ -72,21 +78,18 @@ public class ShuttleScheduleFragment extends Fragment{
         final TextView textView = (TextView) rootView.findViewById(R.id.section_label);
 
         mapWeb = (WebView) rootView.findViewById(R.id.mapWebView);
-//        mapWeb.loadUrl();
         String data = "<body>" + "<img src=\"sfsumapmed.png\"/></body>";
         mapWeb.loadDataWithBaseURL("file:///android_asset/", data, "text/html", "utf-8", null);
 
-//        makeNewTimer();
-
-        timeButton = (Button) rootView.findViewById(R.id.ShuttleButton);
         timerView = (TextView) rootView.findViewById(R.id.timerText);
         timerDescription = (TextView) rootView.findViewById(R.id.shuttleDescription);
-
 //        daySchedule = (TextView) rootView.findViewById(R.id.daySchedule);
 
 
         c = Calendar.getInstance();
-        int day = c.get(Calendar.DAY_OF_WEEK);
+
+        //Picks a schedule based on the day.
+//        int day = c.get(Calendar.DAY_OF_WEEK);
 //        if(day > 1 && day <6){
 //            //Log.d("mon-thu","week");
 //            daySchedule.setText(R.string.weekday_schedule);
@@ -98,14 +101,12 @@ public class ShuttleScheduleFragment extends Fragment{
 //            daySchedule.setText(R.string.whole_schedule);
 //        }
 
-
-
-
+        //Create On-click Listener for the Shuttle Countdown button
+        timeButton = (Button) rootView.findViewById(R.id.ShuttleButton);
         timeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 try {
-                    c = Calendar.getInstance();
                     countdown.cancel();
                 }catch(Exception e){
 
@@ -114,10 +115,21 @@ public class ShuttleScheduleFragment extends Fragment{
             }
         });
 
+        //Create a new Dialog out of the dialog_shuttle_map_info fragment
         mapInfoDialog = new Dialog(getContext());
         mapInfoDialog.setContentView(R.layout.dialog_shuttle_map_info);
         mapInfoDialog.setTitle("Shuttle Map Info");
 
+        //Make the show_map_info button on fragment_shuttle_map expand the dialog
+        showDialog = (Button) rootView.findViewById(R.id.show_map_info);
+        showDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mapInfoDialog.show();
+            }
+        });
+
+        //Make the dismiss_map_dialog button on the dialog_shuttle_map_info dialog dismiss itself
         dismissDialog = (Button) mapInfoDialog.findViewById(R.id.dismiss_map_dialog);
         dismissDialog.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -127,65 +139,94 @@ public class ShuttleScheduleFragment extends Fragment{
         });
 
 
-        showDialog = (Button) rootView.findViewById(R.id.show_map_info);
-        showDialog.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mapInfoDialog.show();
-            }
-        });
-
         return rootView;
     }
 
+    /**
+     * Create new Timer for the Shuttle Countdown.
+     * The timer has 3 use cases:
+     *      1-The button is pressed in the morning before the shuttles start running:
+     *          The timer will display "First shuttle in hh:mm:ss" where "hh:mm:ss" will count down
+     *          the hours:minutes:seconds until the first shuttle arrives
+     *      2-The button is pressed during hours of shuttle operation:
+     *          The timer will display "Shuttles run for hh:mm:ss" where "hh:mm:ss" will count down
+     *          the hours:minutes:seconds until the last shuttle of the day will depart
+     *      3-The button is pressed after the last shuttle is scheduled to depart:
+     *          The timer will display "Out of service 00:00:00" to indicate the timer has run out
+     *          and there will be no more shuttles for the night, or during the weekend
+     *
+     * The Shuttle Schedule will automatically set the first-last shuttle time based on the day
+     * During the weekend there are 0 hours of operation, so the shuttle is always out of service.
+     */
     public void makeNewTimer(){
-        //c = Calendar.getInstance();
+        c = Calendar.getInstance();
+
+        //Create the Current time of day in milliseconds into the day
         long currentTime = makeMs(c.get(Calendar.HOUR_OF_DAY),
                 c.get(Calendar.MINUTE),c.get(Calendar.SECOND));
 
+        //The first shuttle is always scheduled at 7am
         long firstShuttleTime = makeMs(7,0,0);
         long lastShuttleTime;
+
+        //Get current day of week as numerical value
         int day = c.get(Calendar.DAY_OF_WEEK);
 
         //hard coding time for testing scenareos
         //currentTime = makeMs(13,40,41);
         //day = 5;
 
-        if(day > 1 && day <6){
+        //Set the last scheduled Shuttle based on the current day
+        if(day > 1 && day <6){ // MON - THURS SCHEDULE
             //Log.d("mon-thu","week");
             lastShuttleTime = makeMs(22,30,0);
 
-        }else if(day == 6){
+        }else if(day == 6){ //FRIDAY SCHEDULE
             //Log.d("fri","week");
             lastShuttleTime = makeMs(19,15,0);
-        }else{
+        }else{ //WEEKEND SCHEDULE, 0 HOURS OF OPERATION
             //Log.d("weekend","week");
             firstShuttleTime = 0;
             lastShuttleTime = 0;
         }
 
-        if (currentTime<firstShuttleTime){
+        //Compare Current time to First and last shuttle time
+
+        if (currentTime<firstShuttleTime){//BUTTON IS PRESSED BEFORE FIRST SHUTTLE
             //Log.d("before shuttle","week");
             timerDescription.setText("First shuttle in ");
             countdown = new Timer(firstShuttleTime - currentTime, 1000);
             countdown.start();
-        }else if(currentTime < lastShuttleTime){
+        }else if(currentTime < lastShuttleTime){ //BUTTON IS PRESSED DURING HOURS OF OPERATION
             //Log.d("mid shuttle","week");
             timerDescription.setText("Shuttles run for ");
             countdown = new Timer(lastShuttleTime-currentTime,1000);
             countdown.start();
-        }else{
+        }else{  //BUTTON IS PRESSED AFTER HOURS OR ON WEEKEND; OUT OF SERVICE
             //Log.d("after Shuttle","week");
             timerDescription.setText("Out of service");
         }
     }
 
+    /**
+     * Takes the current time of day in hr,min,sec and returns that time as a millisecond value
+     *
+     * @param hr hours into the current day
+     * @param min minutes into the current hour
+     * @param sec seconds into the current minute
+     * @return long representing the current time in milliseconds into the day
+     */
     public long makeMs(int hr, int min, int sec){
         return TimeUnit.HOURS.toMillis(hr)+
                 TimeUnit.MINUTES.toMillis(min)+
                 TimeUnit.SECONDS.toMillis(sec);
 
     }
+
+    /**
+     * Timer class uses Countdown timer to create the Shuttle Countdown
+     * @author Joshua Rubin
+     */
     public class Timer extends CountDownTimer{
 
         /**
@@ -199,6 +240,11 @@ public class ShuttleScheduleFragment extends Fragment{
             super(millisInFuture, countDownInterval);
         }
 
+        /**
+         * Takes the time left on the counter, converts it into hh:mm:ss and displays it on the
+         * timerView in fragment_shuttle_Schedule
+         * @param millisUntilFinished time left on the counter
+         */
         @Override
         public void onTick(long millisUntilFinished) {
             long msRemaining, hrRemaining, minRemaining, secRemaining;
@@ -212,6 +258,9 @@ public class ShuttleScheduleFragment extends Fragment{
 
         }
 
+        /**
+         * When the timer is finished, display 00:00:00 on the timerView in fragment_shuttle_schedule
+         */
         @Override
         public void onFinish() {
             timerView.setText("00:00:00");
